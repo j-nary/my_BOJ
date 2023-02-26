f49dc2e8aad7b8deead17d7a27b78cef31791320
j-nary <dlwls0921@naver.com>
2023-02-27 04:26:23 +0900

    2022 Winter

---
+++ 9654.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21\n        J-Type 327     Light Combat   Unlimited  1\n         NX Cruiser     Medium Fighter Limited    18\n        N1 Starfighter Medium Fighter Unlimited  25\n        Royal Cruiser  Light Combat   Limited    4\n         ";
+}
