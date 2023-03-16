0a799dac26d18dd299d595fa34cb537af30b3bd7
j-nary <dlwls0921@naver.com>
2023-03-16 22:19:12 +0900

    2023 Spring

---
+++ 11719.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+
+    string s;
+    while( getline(cin, s)) {
+        cout << s << '\n';
+    }
+    
+}
