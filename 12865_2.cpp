6c271602f2dd50f9cb01853610bdf8f5e865618b
j-nary <dlwls0921@naver.com>
2023-03-20 13:40:24 +0900

    2023 Spring

--- 12865_2.cpp
+++ 12865_2.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 int dp[2][10000007];
+//dp[i][j] : i번째까지 사용했을 때 무게가 j가 되는 가치의 최댓값
 vector<pair<int, int> > input;
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr);
@@ -21,15 +22,19 @@ int main() {
         input.push_back(make_pair(W, V));
     }
 
-    dp[0][input[0].weight] = input[0].value;    //초기값 설정
-    int res = input[0].value;
+    //초기값 설정
+    dp[0][input[0].weight] = input[0].value;
+    //dp 시작
     for (int i = 1; i < N; i++) {
-        for (int j = 0; j <= K; j++) {
+        for (int j = 1; j <= K; j++) {
             if (j-input[i].weight < 0) dp[i%2][j] = dp[(i-1)%2][j];
             else dp[i%2][j] = max(dp[(i-1)%2][j], dp[(i-1)%2][j-input[i].weight] + input[i].value);
-            if (dp[i%2][j] > res) res = dp[i%2][j];
         }
     }
 
+    int res = 0;
+    for (int i = 0; i <= K; i++) {
+        res = max(res, dp[(N-1)%2][i]);
+    }
     cout << res << '\n';
 }
