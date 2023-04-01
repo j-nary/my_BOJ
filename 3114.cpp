7728ce283d2ef44d7ac5f735d3117a709ba28f99
j-nary <dlwls0921@naver.com>
2023-04-01 22:33:28 +0900

    PS 과제5

--- 3114.cpp
+++ 3114.cpp
@@ -1,4 +1,5 @@
 //BOJ_3114 사과와 바나나 [골드 2]
+//DP로 풀기
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -14,16 +15,46 @@ int main() {
     //입력 받기
     int R, C; cin >> R >> C;
     string input;
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C; j++) {
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
             cin >> input;
             if (input[0] == 'A') {
-                appMap[i][j] = input[1];
+                input.erase(input.begin());
+                appMap[i][j] = stoi(input);
             } else {
-                banMap[i][j] = input[1];
+                input.erase(input.begin());
+                banMap[i][j] = stoi(input);
             }
         }
     }
 
-    
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            banMap[i][j] = banMap[i - 1][j] + banMap[i][j];
+            appMap[i][j] = appMap[i - 1][j] + appMap[i][j];            
+        }
+    }
+
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            if (i == 1 && j == 1) {
+                dp[1][1] = appMap[R][1] - appMap[1][1];
+            }
+            int sum = banMap[i-1][j] + (appMap[R][j] - appMap[i][j]);
+
+            //왼쪽에서 오는 거
+            if (j != 1) dp[i][j] = dp[i][j - 1] + sum;
+
+            //대각선에서 오는 거
+            int tmp = 0;
+            if (j != 1) tmp = dp[i - 1][j - 1] + sum;
+            dp[i][j] = max(dp[i][j], tmp);
+
+            //위쪽에서 오는 거
+            tmp = dp[i - 1][j] - (appMap[i][j] - appMap[i - 1][j]);
+            dp[i][j] = max(dp[i][j], tmp);
+        }
+    }
+
+    cout << dp[R][C] << '\n';
 }
