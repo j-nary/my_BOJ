2b01467d2c374f0b6fc31f91fd24ff03ee934ee8
j-nary <dlwls0921@naver.com>
2023-07-11 22:52:18 +0900

    2023 SCCC Summer

---
+++ 21870.cpp
@@ -0,0 +1,18 @@
+//BOJ_21870 시철이가 사랑한 GCD [골드 5]
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+vector<int> S;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    int N; cin >> N;
+    for (int i = 0; i < N; i++) {
+        int input; cin >> input;
+        S.push_back(input);
+    }
+
+    
+}
