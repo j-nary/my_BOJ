cec849cf42eaa571d240f70778c9261c82eec88c
j-nary <dlwls0921@naver.com>
2024-02-02 00:23:19 +0900

    2023 Winter

---
+++ 16430.cpp
@@ -0,0 +1,14 @@
+//BOJ_16430 제리와 톰 [브론즈 5]
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    int A, B;
+    cin >> A >> B;
+
+    cout << B-A << " " << B << '\n';
+}
