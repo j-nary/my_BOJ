bb4b1ca99db20e276cc5e68aa5bf03bab1b6db2f
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-26 01:23:09 +0900

    티키타카 7주차

---
+++ 15439.cpp
@@ -0,0 +1,14 @@
+//BOJ_15439 베라의 패션 [브론즈 4]
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+  int N;
+  cin >> N;
+
+  cout << N * (N - 1) << '\n';
+}
