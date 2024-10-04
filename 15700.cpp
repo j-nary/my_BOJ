c2505b0dc04d574c34dfa3e09b4e5c505c54bd09
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-05 02:35:36 +0900

    티키타카

---
+++ 15700.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+  long long N, M;
+  cin >> N >> M;
+
+  cout << (N * M) / 2 << '\n';
+}
