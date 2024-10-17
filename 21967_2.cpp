5b56c731019499ca3e95cc58253378eaca970510
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-18 04:19:20 +0900

    티키타카 11주차

---
+++ 21967_2.cpp
@@ -0,0 +1,15 @@
+//BOJ_21967 세워라 반석 위에 [실버 3]
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+  int N; cin >> N;
+  while (--N) {
+    
+  }
+  cout << ans << '\n';
+}
