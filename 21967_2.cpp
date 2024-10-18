d658783c71cfd270a0c98a6b9b33428695e39d70
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-19 00:22:36 +0900

    티키타카 11주차

--- 21967_2.cpp
+++ 21967_2.cpp
@@ -4,12 +4,27 @@
 
 using namespace std;
 
+int cnt[10], ans;
+
+void update(int idx) {
+  ans = max(ans, cnt[idx]);
+  cnt[idx] = 0;
+}
 int main() {
   ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
   int N; cin >> N;
-  while (--N) {
-    
+  while (N--) {
+    int ip; cin >> ip;
+
+    for (int i = 0; i <= 2; i++)
+      if (ip - i > 0) cnt[ip - i]++;
+
+    for (int i = 1; i < ip - 2; i++) update(i);
+
+    for (int i = ip + 1; i < 10; i++) update(i);
   }
+
+  for (int i = 1; i < 10; i++) ans = max(ans, cnt[i]);
   cout << ans << '\n';
 }
