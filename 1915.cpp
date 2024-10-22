febed22852d50c5fd6dd4af58d33079081ba6018
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-23 01:03:42 +0900

    2024 Autumn

--- 1915.cpp
+++ 1915.cpp
@@ -4,16 +4,27 @@
 
 using namespace std;
 
-char arr[1003][1003];
+int dp[1003][1003];
+int Arr[1003][1003];
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
     int N, M; cin >> N >> M;
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= M; j++) {
-            cin >> arr[i][j];
+            char ip; cin >> ip;
+            Arr[i][j] = ip - '0';
         }
     }
 
-    
+    int ans = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            if (!Arr[i][j]) continue;
+            Arr[i][j] = min({Arr[i - 1][j - 1], Arr[i - 1][j], Arr[i][j - 1]}) + 1;
+            ans = max(ans, Arr[i][j]);
+        }
+    }
+
+    cout << ans * ans << '\n';
 }
