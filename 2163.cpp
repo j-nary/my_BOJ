150e3d448ebc19b0211cf2476b71eda164527228
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-12-31 05:45:23 +0900

    2024 Winter

---
+++ 2163.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int N, M;
+  cin >> N >> M;
+  cout << N * M - 1 << '\n';
+}
