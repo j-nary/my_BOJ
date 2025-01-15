1d95e71ef3a39e3675bc0bdcef929f4961259d16
Lee jin <83453646+j-nary@users.noreply.github.com>
2025-01-16 02:51:36 +0900

    Backtracking

--- 9663.cpp
+++ 9663.cpp
@@ -4,8 +4,28 @@
 
 using namespace std;
 
+int N, ans;
+int loc[20];
+
+void backtracking(int depth) {
+    if (depth == N) { ans++; return; }
+
+    for (int i = 0; i < N; i++) {
+        loc[depth] = i;
+        bool flag = true;
+        for (int j = 0; j < depth; j++) {
+            if (loc[depth] == loc[j] || abs(loc[depth] - loc[j]) == depth - j) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) backtracking(depth + 1);
+    }
+}
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-    int N; cin >> N;
+    cin >> N;
+    backtracking(0);
+    cout << ans << '\n';
 }
